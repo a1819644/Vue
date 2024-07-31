dc2d262c4a4ff451880b2470cf60ade64325452e
a1819644 <nicky1234567kumar@gmail.com>
2024-07-31 18:33:01 +1000

    This section done

--- app.js
+++ app.js
@@ -7,22 +7,32 @@ const app = Vue.createApp({
                     title: 'The lord of the world',
                     name: 'SJ',
                     age: '45',
-                    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51g1gMBrZN3FbN17flaY_YSYmTMGuudh3Q&s'
+                    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51g1gMBrZN3FbN17flaY_YSYmTMGuudh3Q&s',
+                    isFav:true
                 },
                 {
                     title: 'World of books',
                     name: 'SJ',
                     age: '45',
-                    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51g1gMBrZN3FbN17flaY_YSYmTMGuudh3Q&s'
+                    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw51g1gMBrZN3FbN17flaY_YSYmTMGuudh3Q&s',
+                    isFav: false
                 },
                 {
                     title: 'The lord',
                     name: 'SAM',
                     age: '45',
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsfxrcUtlaLqSTTpA7N9cWKIopvRNtXngM2A&s'
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsfxrcUtlaLqSTTpA7N9cWKIopvRNtXngM2A&s',
+                    isFav:false
                 },
             ]
         }
+    },
+
+    methods:{
+        bookmark(book){
+            book.isFav= !book.isFav;
+            
+        },
     }
 
 });
